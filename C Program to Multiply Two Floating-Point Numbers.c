42dce1b4ed030c6a77aa183ae8147f101f1a1050
Soham-bobade9764 <147583117+Soham-bobade9764@users.noreply.github.com>
2023-10-12 09:53:17 +0530

    Update C Program to Multiply Two Floating-Point Numbers.c

--- C Program to Multiply Two Floating-Point Numbers.c
+++ C Program to Multiply Two Floating-Point Numbers.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-int main() {    
+int main() {
+    double a, b, product;
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &a, &b);  
+ 
+    // Calculating product
+    product = a * b;
 
-    int number1, number2, sum;
+    // %.2lf displays number up to 2 decimal point
+    printf("Product = %.2lf", product);
     
-    printf("Enter two integers: ");
-    scanf("%d %d", &number1, &number2);
-
-    // calculate the sum
-    sum = number1 + number2;      
-    
-    printf("%d + %d = %d", number1, number2, sum);
     return 0;
 }
+

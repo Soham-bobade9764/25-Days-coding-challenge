2e05cd7224460b71566010562bcd10a6d0cfbd8d
Soham-bobade9764 <147583117+Soham-bobade9764@users.noreply.github.com>
2023-10-19 09:38:42 +0530

    Update 22.C Program to Find LCM of two Numbers.c

--- 22.C Program to Find LCM of two Numbers.c
+++ 22.C Program to Find LCM of two Numbers.c
@@ -4,7 +4,7 @@ int main() {
 
     int n1, n2, max;
 
- printf("Enter two positive integers: ");
+printf("Enter two positive integers: ");
 scanf("%d %d", &n1, &n2);
 
 // maximum number between n1 and n2 is stored in max
@@ -12,7 +12,8 @@ max = (n1 > n2) ? n1 : n2;
 
  while (1) {
   if ((max % n1 == 0) && (max % n2 == 0)) {
-  printf("The LCM of %d and %d is %d.", n1, n2, max);
+  printf("The LCM of %d and %d is %d.", 
+      n1, n2, max);
       break;
         }
         ++max;
